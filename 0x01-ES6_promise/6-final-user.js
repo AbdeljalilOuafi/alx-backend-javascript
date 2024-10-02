a666c0356928d7495dd5265125bffc9dc4475801
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-02 23:18:42 +0100

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,19 +1,11 @@
-#!/usr/bin/node
-
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      const returnArray = [];
-      results.forEach((result) => {
-        if (result.status === 'fulfilled') {
-          returnArray.push({ status: result.status, value: result.value });
-        } else if (result.status === 'rejected') {
-          returnArray.push({ status: result.status, value: result.reason });
-        }
-      });
-      return returnArray;
-    });
+export default function handleProfileSignup(firstName, lastName, photo) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(photo)])
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason.toString(),
+    })));
 }
