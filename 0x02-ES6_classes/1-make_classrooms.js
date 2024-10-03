ae144703b3ebc4855833d325a72c73c7c934dc71
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-03 21:40:39 +0100

    better_solution

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,17 +1,8 @@
 #!/usr/bin/node
 
-import ClassRoom from './0-classroom'
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const classRooms = [];
-  for (let i = 0; i < 3; i++) {
-    if (i === 0) {
-      classRooms.push(new ClassRoom(19));
-    } else if (i === 1) {
-      classRooms.push(new ClassRoom(20));
-    } else if (i === 2) {
-      classRooms.push(new ClassRoom(34));
-    }
-  }
-  return classRooms;
+  const sizes = [19, 20, 34];
+  return sizes.map(size => new ClassRoom(size));
 }
