22ba0a627e2b70828725aba78862dd8856cdc9d3
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-04 00:16:10 +0100

    5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number' || Number.isNaN(sqft)) {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number' || Number.isNaN(value)) {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
