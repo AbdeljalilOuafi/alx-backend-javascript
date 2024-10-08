8767e86434d9e49596cc5c8361ce810822e92982
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-08 03:22:14 +0100

    7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-main.js
+++ 0x03-ES6_data_manipulation/7-main.js
@@ -1,4 +1,5 @@
-// 7-main.js
+import hasValuesFromArray from "./7-has_array_values.js";
 
-// This is the main file for the solution
-
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
+console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
