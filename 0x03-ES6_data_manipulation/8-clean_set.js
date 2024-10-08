6a9caed3bbde4fe6fb348432a6e56361195e9fe4
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-10-08 03:49:57 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+export default function cleanSet(set, startString) {
+  const arr = [];
+  for (const str of set) {
+    if (str.startsWith(startString)) {
+      arr.push(str.split(startString)[1]);
+    }
+  }
+  return arr.join('-');
+}
+
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
