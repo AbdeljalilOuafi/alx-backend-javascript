fb2cb8d18d996eff5b1d17a1b4fd5d0b39c2b85a
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-11-26 16:40:14 +0100

    linter_problems_again

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,7 +3,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString()}`);
-  process.stdin.pause()
+  process.stdin.pause();
 });
 
 process.stdin.on('pause', () => {
