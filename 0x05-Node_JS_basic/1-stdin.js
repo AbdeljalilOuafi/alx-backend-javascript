aabbf3053b35aa908bd59a2ec3a91df464a51dd8
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-11-26 16:32:23 +0100

    fix_lint_problems

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString()}`);
-  process.exit()
-})
+  process.exit();
+});
 
 process.on('exit', () => {
-  console.log("This important software is now closing");
-})
+  console.log('This important software is now closing');
+});
