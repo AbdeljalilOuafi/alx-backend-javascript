8cd1f73cc7cb69300b7f2ac8d20b3453ef52ce2f
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-11-26 21:07:02 +0100

    4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+
+const http = require('http');
+
+const port = 1245;
+const app = http.createServer((req, res) => {
+  res.write('Hello Holberton School!');
+  res.end();
+});
+
+app.listen(port);
