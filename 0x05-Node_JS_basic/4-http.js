d7bc71ab9fef07ff9652c1d6ef3616772465aa10
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-11-26 21:35:14 +0100

    5-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,8 +4,7 @@ const http = require('http');
 
 const port = 1245;
 const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port);
