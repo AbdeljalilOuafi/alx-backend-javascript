3d7a23547e4d1c8785bc3456713ecd62aaf5f49e
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-11-27 16:15:27 +0100

    full_server

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+import express from 'express';
+import AppController from '../controllers/AppController';
+import StudentsController from '../controllers/StudentsController';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  AppController.getHomepage(req, res);
+});
+
+app.get('/students', (req, res) => {
+  StudentsController.getAllStudents(req, res);
+});
+
+app.get('/students/:major', (req, res) => {
+  StudentsController.getAllStudentsByMajor(req, res);
+});
+
+export default app;
--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+import app from './routes/index';
+
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server running at localhost:${port}/`);
+});
